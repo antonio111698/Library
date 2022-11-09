9a9efe8ceaff320307e0e1cddc76981b65ae42a7
antonio_artimon@yahoo.com <Artimonmaria1723?>
2022-11-09 16:10:54 +0200

    Book description edited

--- app/src/main/java/com/antonio/mylibrary/Utils.java
+++ app/src/main/java/com/antonio/mylibrary/Utils.java
@@ -33,7 +33,8 @@ public class Utils {
             initData();
         }
 
-         SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         Gson gson = new Gson();
 
         if(null == getAlreadyReadBooks()){
@@ -59,16 +60,29 @@ public class Utils {
     }
 
     private void initData() {
-        //TODO: add initial data
         ArrayList<Book> books = new ArrayList<>();
         books.add(new Book(1,"On the Edge","Alison Levine",321,
                 "https://m.media-amazon.com/images/I/81yhdZV3zLL.jpg",
                 "Leadership lessons from Mount Everest and other extreme environments",
-                "long desc"));
+                "Extreme athlete Alison Levine describes the leadership skills that helped her reach the summit of Mt. Everest and explains how the same skills can help you navigate today’s tumultuous, fast-changing business environment. " +
+                        "She shows why leadership fundamentals like teamwork, flexibility and agility are essential whether you’re contending with a blizzard in the Himalayas or handling a disruption in the technology market. This is an unusual business manual." +
+                        " Instead of flow charts, Levine offers tableaux of subzero temperatures and perilous precipices in exotic locales like Kathmandu, Indonesia and Antarctica. You’ve heard her leadership concepts before: Learn from your failures; guard against complacency;" +
+                        " break out of your comfort zone. But, these ideas take on new vibrancy against the backdrop of the world’s highest peaks. Levine’s friendly, chatty voice sparks her vivid storytelling, which is peppered with earthy wisecracks." +
+                        " getAbstract recommends her business manual-slash-travelogue to adventurous managers and entrepreneurs who want to push their boundaries."));
         books.add(new Book(2,"Robinson Crusoe","Daniel Defoe",231,
                 "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1631057329l/2932._SY475_.jpg",
-                "Robinson Crusoe este un roman scris de Daniel Defoe, publicat pentru prima dată în 1719 și este adesea considerat primul roman scris vreodată în limba engleză.",
-                "long desc"));
+                "The Life and Strange Surprizing Adventures of Robinson Crusoe, of York, Mariner: Who lived Eight and Twenty Years, all alone in an un-inhabited Island on the Coast of America, near the Mouth of the Great River of Oroonoque;" +
+                        " Having been cast on Shore by Shipwreck, wherein all the Men perished but himself." +
+                        " With An Account how he was at last as strangely deliver'd by Pyrates. Written by Himself.",
+                "Robinson Crusoe is an Englishman from the town of York in the seventeenth century, the youngest son of a merchant of German origin." +
+                        " Encouraged by his father to study law, Crusoe expresses his wish to go to sea instead. His family is against Crusoe going out to sea, and his father " +
+                        "explains that it is better to seek a modest, secure life for oneself. Initially, Robinson is committed to obeying his father, but he eventually succumbs to" +
+                        " temptation and embarks on a ship bound for London with a friend. When a storm causes the near deaths of Crusoe and his friend, the friend is dissuaded from " +
+                        "sea travel, but Crusoe still goes on to set himself up as merchant on a ship leaving London. This trip is financially successful, and Crusoe plans another, leaving" +
+                        " his early profits in the care of a friendly widow. The second voyage does not prove as fortunate: the ship is seized by Moorish pirates, and Crusoe is enslaved to" +
+                        " a potentate in the North African town of Sallee. While on a fishing expedition, he and a slave boy break free and sail down the African coast." +
+                        " A kindly Portuguese captain picks them up, buys the slave boy from Crusoe, and takes Crusoe to Brazil. In Brazil, Crusoe establishes himself as a plantation owner and soon becomes successful." +
+                        " Eager for slave labor and its economic advantages, he embarks on a slave-gathering expedition to West Africa but ends up shipwrecked off of the coast of Trinidad."));
 
         SharedPreferences.Editor editor = sharedPreferences.edit();
         Gson gson = new Gson();
